8402506b5775c485d565a012b51a4de3ce8327e3
Vontus <albertomk8@gmail.com>
2017-07-23 01:26:10 +0200

    Add vault

--- src/main/java/vontus/magicbottle/Plugin.java
+++ src/main/java/vontus/magicbottle/Plugin.java
@@ -5,8 +5,10 @@ import java.util.logging.Logger;
 
 import org.bstats.bukkit.Metrics;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.economy.Economy;
 import vontus.magicbottle.Commands;
 import vontus.magicbottle.Events;
 import vontus.magicbottle.config.Config;
@@ -16,10 +18,12 @@ public class Plugin extends JavaPlugin {
 	public static Logger logger;
 	public HashSet<Player> autoEnabled = new HashSet<>();
 	public Metrics metrics;
+	Economy econ = null;
 
 	@Override
 	public void onEnable() {
 		logger = getLogger();
+		setupEconomy();
 		loadConfig();
 		new Recipes(this);
 		this.getServer().getPluginManager().registerEvents(new Events(this), this);
@@ -33,5 +37,22 @@ public class Plugin extends JavaPlugin {
 		this.saveDefaultConfig();
 		new Config(this);
 		new Messages(this);
+		
+		if (Config.costMoneyCraftNewBottle != 0 && econ == null) {
+			logger.warning("Vault is required to add a cost to crafting a bottle. Set the cost to 0 to disable this warning.");
+			Config.costMoneyCraftNewBottle = 0;
+		}
+	}
+
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
 	}
 }

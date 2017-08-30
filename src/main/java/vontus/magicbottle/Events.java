af63d2dc33d9b4563940a962cab782ab70fbcf6b
Vontus <>
2017-08-30 13:57:58 +0200

    Bug fixing
    Experience wasn't displayed properly when using the recipe to deposit.

--- src/main/java/vontus/magicbottle/Events.java
+++ src/main/java/vontus/magicbottle/Events.java
@@ -29,23 +29,23 @@ import vontus.magicbottle.util.Exp;
 import vontus.magicbottle.util.Utils;
 
 public class Events implements Listener {
-    private HashSet<UUID> wait;
-    private Plugin plugin;
+	private HashSet<UUID> wait;
+	private Plugin plugin;
 
-    public Events(Plugin plugin) {
-        this.plugin = plugin;
-        this.wait = new HashSet<>();
-    }
-    
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onClickInventory(InventoryClickEvent e) {
-    	InventoryType invType = e.getView().getType();
-    	if (invType == InventoryType.ANVIL || invType == InventoryType.BREWING) {
-    		e.setCancelled(MagicBottle.isMagicBottle(e.getCurrentItem()));
-    	}
-    }
+	public Events(Plugin plugin) {
+		this.plugin = plugin;
+		this.wait = new HashSet<>();
+	}
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onClickInventory(InventoryClickEvent e) {
+		InventoryType invType = e.getView().getType();
+		if (invType == InventoryType.ANVIL || invType == InventoryType.BREWING) {
+			e.setCancelled(MagicBottle.isMagicBottle(e.getCurrentItem()));
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		Action act = event.getAction();
@@ -93,21 +93,22 @@ public class Events implements Listener {
 			MagicBottle result = new MagicBottle(e.getRecipe().getResult());
 			if (MagicBottle.isMagicBottle(getFirstIngredient(e.getInventory()))) {
 				if (result.isEmpty()) {
-					onRecipeDeposit(e);
+					onRecipeWithdraw(e);
 				} else {
-					onRecipeFill(e);
+					onRecipeDeposit(e);
 				}
 			} else {
 				if (MagicBottle.isMagicBottle(e.getRecipe().getResult())) {
 					if (isEmptyBottleRecipe(e.getInventory())) {
 						Player player = (Player) e.getView().getPlayer();
-						if (player.hasPermission(Config.authorizationCraft)) {
+						if (player.hasPermission(Config.permCraft)) {
 							if (chargeNewBottleMoney(player)) {
 								e.getInventory().setResult(new MagicBottle(0).getItem());
 								SoundEffect.newBottle(player);
 							} else {
 								SoundEffect.forbidden(player);
-								player.sendMessage(Messages.msgNotEnoughMoney.replace(Messages.moneyReplacer, Double.toString(Config.costMoneyCraftNewBottle)));
+								player.sendMessage(Messages.msgNotEnoughMoney.replace(Messages.moneyReplacer,
+										Double.toString(Config.costMoneyCraftNewBottle)));
 								e.setCancelled(true);
 							}
 						} else {
@@ -142,15 +143,15 @@ public class Events implements Listener {
 	public void onPlayerLeave(PlayerQuitEvent e) {
 		plugin.autoEnabled.remove(e.getPlayer());
 	}
-	
+
 	@EventHandler
 	public void onPlayerKicked(PlayerKickEvent e) {
 		plugin.autoEnabled.remove(e.getPlayer());
 	}
 
-    private void onInteractDeposit(MagicBottle bottle, Player p) {
+	private void onInteractDeposit(MagicBottle bottle, Player p) {
 		if (Exp.getPoints(p) > 0) {
-			if (p.hasPermission(Config.authorizationDeposit)) {
+			if (p.hasPermission(Config.permDeposit)) {
 				int round = p.isSneaking() ? 1 : 0;
 				int targetPlayerLevel = Exp.floorLevel(p, round);
 				int expToDeposit = Exp.getExpToLevel(p, targetPlayerLevel) * -1;
@@ -163,7 +164,7 @@ public class Events implements Listener {
 
 	private void onInteractWithdraw(MagicBottle bottle, Player p) {
 		if (bottle.getExp() > 0) {
-			if (p.hasPermission(Config.authorizationWithdraw)) {
+			if (p.hasPermission(Config.permWithdraw)) {
 				int round = p.isSneaking() ? 1 : 0;
 				int targetPlayerLevel = Exp.ceilingLevel(p, round);
 				int expToWithdraw = Exp.getExpToLevel(p, targetPlayerLevel);
@@ -177,26 +178,29 @@ public class Events implements Listener {
 
 	private void onPrepareRecipeWithdraw(PrepareItemCraftEvent e) {
 		Player player = (Player) e.getView().getPlayer();
-		if (!Config.recipePour || !player.hasPermission(Config.authorizationWithdraw)) {
+		if (!Config.recipePour || !player.hasPermission(Config.permWithdraw)) {
 			e.getInventory().setResult(null);
 		}
 	}
 
 	private void onPrepareRecipeDeposit(PrepareItemCraftEvent e) {
 		Player player = (Player) e.getView().getPlayer();
-		if (Config.recipeFill && player.hasPermission(Config.authorizationDeposit) && Exp.getPoints(player) > 0) {
+		if (Config.recipeFill && player.hasPermission(Config.permDeposit) && Exp.getPoints(player) > 0) {
 			MagicBottle bottle = new MagicBottle(0);
-			bottle.setExp(bottle.getMaxFillablePoints(player, Exp.getPoints(player)));
+			int playerPoints = Exp.getPoints(player);
+			int expCost = (int) Math.round(playerPoints * Config.costPercentageDeposit);
+			int maxPoints = bottle.getMaxFillablePoints(player, playerPoints - expCost);
+			bottle.setExp(maxPoints);
 			e.getInventory().setResult(bottle.getItem());
 		} else {
 			e.getInventory().setResult(null);
 		}
 	}
 
-	private void onRecipeDeposit(CraftItemEvent e) {
+	private void onRecipeWithdraw(CraftItemEvent e) {
 		Player player = (Player) e.getView().getPlayer();
 		ItemStack i = getFirstIngredient(e.getInventory());
-		if (i.getAmount() == 1 && Config.recipePour && player.hasPermission(Config.authorizationWithdraw)) {
+		if (i.getAmount() == 1 && Config.recipePour && player.hasPermission(Config.permWithdraw)) {
 			MagicBottle bottle = new MagicBottle(i);
 			bottle.withdraw(player, bottle.getExp());
 			e.getInventory().setResult(new MagicBottle(0).getItem());
@@ -205,10 +209,11 @@ public class Events implements Listener {
 		}
 	}
 
-	private void onRecipeFill(CraftItemEvent e) {
+	private void onRecipeDeposit(CraftItemEvent e) {
 		Player player = (Player) e.getView().getPlayer();
-		ItemStack ingredient = getFirstIngredient(e.getInventory());
-		if (ingredient.getAmount() == 1 && Exp.getPoints(player) > 0 && player.hasPermission(Config.authorizationDeposit) && Config.recipeFill) {
+		ItemStack ingr = getFirstIngredient(e.getInventory());
+		if (ingr.getAmount() == 1 && Exp.getPoints(player) > 0 && player.hasPermission(Config.permDeposit)
+				&& Config.recipeFill) {
 			MagicBottle bottle = new MagicBottle(0);
 			bottle.deposit(player, player.getTotalExperience());
 			e.getInventory().setResult(bottle.getItem());
@@ -218,9 +223,10 @@ public class Events implements Listener {
 	}
 
 	private ItemStack getFirstIngredient(CraftingInventory inv) {
-		for(ItemStack i : inv.getMatrix()) {
-			if (i != null && i.getType() != Material.AIR)
+		for (ItemStack i : inv.getMatrix()) {
+			if (Utils.getMaterial(i) != Material.AIR) {
 				return i;
+			}
 		}
 		return null;
 	}
@@ -228,19 +234,19 @@ public class Events implements Listener {
 	private boolean timeOut(Player p) {
 		if (!wait.contains(p.getUniqueId())) {
 			wait.add(p.getUniqueId());
-		    new BukkitRunnable(){
-		
-		        @Override
+			new BukkitRunnable() {
+
+				@Override
 				public void run() {
-		        	wait.remove(p.getUniqueId());
-		        }
-		    }.runTaskLater(this.plugin, 3);
-		    return true;
+					wait.remove(p.getUniqueId());
+				}
+			}.runTaskLater(this.plugin, 3);
+			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	private boolean isEmptyBottleRecipe(CraftingInventory inv) {
 		for (int i = 0; i < 9; i++) {
 			ItemStack item = inv.getMatrix()[i];
@@ -251,9 +257,9 @@ public class Events implements Listener {
 		}
 		return true;
 	}
-	
+
 	private boolean chargeNewBottleMoney(Player p) {
-		if (Config.costMoneyCraftNewBottle != 0) {
+		if (Config.costMoneyCraftNewBottle != 0 && !p.hasPermission(Config.permCraftCostExempt)) {
 			return plugin.econ.withdrawPlayer(p, Config.costMoneyCraftNewBottle).transactionSuccess();
 		} else {
 			return true;
